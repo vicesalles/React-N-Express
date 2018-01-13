2b82b9e9bb1a8c3f2b869ec870de2eff05a0a785
Vice Sallés <vicenc.salles@gmail.com>
2018-01-13 11:01:10 +0100

    feat: initing google authentication

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send({hi:'there'});
-});
+passport.use(new googleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+        console.log('token: ', accessToken);
+    }
+));
 
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
